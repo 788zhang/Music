c4f7b13828f2cae268ac4e00ae554942f6fa5eb0
赵佳慧--芒果iOS <1146623752@qq.com>
2015-12-27 15:39:11 +0800

    赵佳慧

--- zhangAndZhao/zhangAndZhao/AppDelegate.m
+++ zhangAndZhao/zhangAndZhao/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "ZhaoViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -18,6 +18,13 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    ZhaoViewController *zhaoVc = [[ZhaoViewController alloc]init];
+    UINavigationController *nav = [[UINavigationController alloc]initWithRootViewController:zhaoVc];
+    self.window.rootViewController = nav;
+    
+    
+    
+    
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
---
+++ zhangAndZhao/zhangAndZhao/ZhaoViewController.h
@@ -0,0 +1,13 @@
+//
+//  ZhaoViewController.h
+//  zhangAndZhao
+//
+//  Created by scjy on 15/12/27.
+//  Copyright © 2015年 scjy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZhaoViewController : UIViewController
+
+@end
